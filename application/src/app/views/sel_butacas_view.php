be12d97fcc5e5cf5e3fb96523e4f9a40f872d6e9
Dieg <diegomarcello_17@hotmail.com>
2021-07-07 16:08:51 -0300

    agregadas las clases e ids en el svg
    Eliminados archivos del otro proyecto y comienzo controlador de peliculas

---
+++ application/src/app/views/sel_butacas_view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+
+<head>
+    <?php
+    require 'parts/head_view.php'
+    ?>
+    <link rel="stylesheet" type='text/css' href="assets/css/index.css" />
+    <link rel="stylesheet" type='text/css' href="assets/css/main.css" />
+    <link rel="stylesheet" type='text/css' href="assets/css/sel_butacas.css" />
+</head>
+
+<body>
+    <?php
+    require 'parts/header_view.php';
+    ?>
+
+    <main>
+        <section class='butacas-container'>
+            <?php include("resources/butacas.svg"); ?>
+        </section>
+        <section>
+            
+        </section>
+    </main>
+    <?php
+    require 'parts/footer_view.php';
+    ?>
+</body>
+
+</html>
