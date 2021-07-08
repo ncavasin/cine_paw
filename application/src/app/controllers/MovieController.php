d84f67c962b6e0ccca1e75cc195566e234445270
Dieg <diegomarcello_17@hotmail.com>
2021-07-08 02:15:09 -0300

    pantalla de confirmar orden hecha

--- application/src/app/controllers/MovieController.php
+++ application/src/app/controllers/MovieController.php
@@ -8,6 +8,6 @@ use const Paw\core\database\FILE_SIZE_MAX;
 
 class MovieController extends Controller {
     public function movieInfo() {
-        require $this->viewsDir . 'sel_tickets_view.php';
+        require $this->viewsDir . 'confirm_order_view.php';
     }
 }
---
+++ application/src/app/views/confirm_order_view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+
+<head>
+    <?php
+    require 'parts/head_view.php'
+    ?>
+    <link rel="stylesheet" type='text/css' href="assets/css/index.css" />
+    <link rel="stylesheet" type='text/css' href="assets/css/main.css" />
+    <link rel="stylesheet" type='text/css' href="assets/css/sel_tickets.css" />
+</head>
+
+<body>
+    <?php
+    require 'parts/header_view.php';
+    ?>
+    <main>
+        <h2>Resumen de compra</h2>
+        <section class='content card'>
+            <h3>Iron Man 4</h3>
+            <p>3 entradas generales</p>
+            <p>1 entrada niños</p>
+            <p>Butacas: E5, E6, E7</p>
+            <p>Total: $1700</p>
+        </section>
+    </main>
+    <?php
+    require 'parts/footer_view.php';
+    ?>
+</body>
+
+</html>
