d54de29acd8daeaa1949f7d593dba107d5b5de81
ncavasin <ncavasin97@gmail.com>
2021-07-08 02:22:19 -0300

    fix acople a modelos

--- application/src/app/controllers/MovieController.php
+++ application/src/app/controllers/MovieController.php
@@ -3,8 +3,11 @@
 namespace Paw\app\controllers;
 
 use Paw\core\Controller;
+use Paw\app\models\Movie;
 
 class MovieController extends Controller {
+    public ?string $modelName = Movie::class;
+
     public function movieInfo() {
         require $this->viewsDir . 'sel_tickets_view.php';
     }
--- application/src/app/controllers/PaymentController.php
+++ application/src/app/controllers/PaymentController.php
@@ -3,8 +3,11 @@
 namespace Paw\app\controllers;
 
 use Paw\core\Controller;
+use Paw\app\models\Payment;
 
 class PaymentController extends Controller {
+    public ?string $modelName = Payment::class;
+    
     public function paymentInfo() {
         require $this->viewsDir . 'payment_view.php';
     }
